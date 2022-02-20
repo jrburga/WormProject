ca86bf89e90ccc6e0d65ab01791ecb68fa95302f
Jake Burga <jakerburga@gmail.com>
2022-02-20 15:11:55 -0500

    rigged worm with polygon2D textures
    Wrote up some auto generation for rigging a worm with 2d polygons. Also, looks like I moved some files around

---
+++ Tests/ViewportCapture.gd
@@ -0,0 +1,42 @@
+tool
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export(NodePath) var viewport_path : NodePath;
+export(NodePath) var texture_rect : NodePath
+export(bool) var generate_texture = false setget _set_generate_texture, _get_generate_texture
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	generate_texture
+	print(viewport_path)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _set_generate_texture(value):
+	if value == true:
+		print('generating texture')
+		var viewport = get_node(viewport_path) as Viewport
+		var rect = get_node(texture_rect) as TextureRect
+		if not viewport:
+			return
+			
+		viewport.set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
+		
+		var img = viewport.get_texture().get_data()
+		
+		img.flip_y()
+		
+		var tex = ImageTexture.new()
+		tex.create_from_image(img)
+		
+		rect.set_texture(tex)
+func _get_generate_texture():
+	return false
