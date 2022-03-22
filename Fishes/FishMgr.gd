f9ff2a85b9057858b683a19c1242c705eee5e4f4
Jake Burga <jakerburga@gmail.com>
2022-03-21 23:09:18 -0400

    use spawn lines instead of points

--- Fishes/FishMgr.gd
+++ Fishes/FishMgr.gd
@@ -13,17 +13,20 @@ func _spawn_fish_random():
 	if Fish:
 		var new_fish = Fish.instance() as Node2D
 		
-		var num_spawn_positions = $SpawnPositions.get_child_count()
-		var spawn_idx = randi() % num_spawn_positions
+		var spawn_position = Vector2()
 		
-		var spawn_position = $SpawnPositions.get_child(spawn_idx) as Position2D
-		
+		var path_idx = randi() % 2
+		if path_idx == 0:
+			spawn_position = ($SpawnPath2D_0 as Path2D).curve.interpolate(0, randf())
+		if path_idx == 1:
+			spawn_position = ($SpawnPath2D_1 as Path2D).curve.interpolate(0, randf())
+			
 		var fish_velocity = Vector2()
-		if spawn_position.position.x > 0:
+		if spawn_position.x > 0:
 			fish_velocity = Vector2(-200, 0)
 		else:
 			fish_velocity = Vector2(200, 0)
-		new_fish.global_position = spawn_position.global_position
+		new_fish.global_position = spawn_position
 		new_fish.velocity = fish_velocity
 		add_child(new_fish)
 		
